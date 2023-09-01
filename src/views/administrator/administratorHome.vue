f1d1a60fffbc14f1f2dfcee65f749e8c16a99649
jiashuai <442908218@qq.com>
2023-09-01 10:43:16 +0800

    first

---
+++ src/views/administrator/administratorHome.vue
@@ -0,0 +1,19 @@
+<template>
+  <div>
+    <div class="hander">
+        
+    </div>
+    <div class="content-left"></div>
+    <div class="content-right"></div>
+  </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
